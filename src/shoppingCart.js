a25edeeacd6bdd5606334605bc18bbdef7e8b006
JoyceTerra <joyterra@gmail.com>
2018-08-18 14:37:01 +0200

    3/4 tests passing on first assignment

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,42 +1,35 @@
 class ShoppingCart{
-    constructor(name, quantity, price){
-        this.name = name,
-        this.quantity = quantity,
-        this.price = price
-      
+    constructor(){
+        this.item = []
     }
 
     getItems(){
-        // let cartStorage = [];
-       //this.list.push(item)
-    
-        //return []
+        return this.item
     }
     
-    addItem(newName, newQuantity, newPrice){
-        //cartStorage.push(newName, newQuantity, newPrice)
-       // console.log(this.newName + ' is now added to the cart')
+    addItem(nameItem, quantityItem, priceItem){
+       this.item.push({
+           name: nameItem,
+           quantity: quantityItem,
+           pricePerUnit: priceItem
+       })
     }
-
+    
     clear(){
+        this.item = []
+    }
+    clone(){
 
     }
-    // clone(){
-
-    // }
 }
 
 
-let cart = new ShoppingCart('iPhone', 3, 12)
-//console.log(cart)
-cart.getItems(); //empty array ----- After adding items, a call to `cart.getItems()` should return the added items in object form
-cart.addItem('nominho', 4, 55);
-//console.log(cart.addItem());
-//which adds a new item to an internal array. 
+let cart = new ShoppingCart("ahoahi", 32, 45)
+cart.getItems();
+cart.addItem("Trash can", 1, 15.5);
 cart.clear();
-//should remove all items from the items array.
-// cart.clone()//should return a _new_ `ShoppingCart` object that contains all the same items. 
-// //However, the items array and all the items inside should be **copied** so that any changes to one of the carts does not affect the other.
+
+console.log("AAAAAAAAAAAAAAA", cart)
 
 
-module.exports = { ShoppingCart }
+module.exports =  ShoppingCart
